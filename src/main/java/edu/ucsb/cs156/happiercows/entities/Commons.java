23a5f7487f3cd015fbd88f1afe15c340ac18b1ee
MrJacobEisner <mrjeisner@gmail.com>
2023-06-02 12:18:24 -0700

    Implemented pricechange methods and attributes

--- src/main/java/edu/ucsb/cs156/happiercows/entities/Commons.java
+++ src/main/java/edu/ucsb/cs156/happiercows/entities/Commons.java
@@ -32,6 +32,13 @@ public class Commons
   private double degradationRate;
   private boolean showLeaderboard;
   private int carryingCapacity;
+  private double priceChange;
+  public void increaseCowPrice() {
+    cowPrice += priceChange;
+  }
+  public void decreaseCowPrice() {
+    cowPrice -= priceChange;
+  }
 
   @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST,CascadeType.REMOVE})
   @JoinTable(name = "user_commons",
