91faea006329a6fce7dca55e46223c44003bc3a7
Esmé Puzio <esmepuzio@Esmes-MacBook-Pro.local>
2023-06-06 13:20:33 -0700

    Removed exception for when commons not started - wasn't needed

--- src/main/java/edu/ucsb/cs156/happiercows/errors/CommonsNotStartedException.java
+++
@@ -1,7 +0,0 @@
-package edu.ucsb.cs156.happiercows.errors;
-
-public class CommonsNotStartedException extends Exception {
-    public CommonsNotStartedException(String messageString){
-      super(messageString);
-    }
-  }
