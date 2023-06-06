6b2cd502278222f4c94716ba2146e20760acce46
Esmé Puzio <esmepuzio@169-231-110-144.wireless.ucsb.edu>
2023-06-05 22:15:54 -0700

    Removed stryker tmp files

--- .stryker-tmp/sandbox2769032/frontend/src/main/components/Jobs/InstructorReportForm.js
+++
@@ -1,8 +0,0 @@
-function InstructorReportForm() {
-    return (
-      <p>This will be the Instructor Report Function (coming soon)</p>
-    );
-  }
-  
-  export default InstructorReportForm;
-  
--- .stryker-tmp/sandbox8228531/frontend/src/main/utils/dateUtils.js
+++
@@ -1,9 +0,0 @@
-
-const padWithZero = (n) => { return n < 10 ? '0' + n : n; }
-
-const timestampToDate = (timestamp) => {
-    var date = new Date(timestamp);
-    return (date.getFullYear() + "-" + (padWithZero(date.getMonth()+1)) + "-" + padWithZero(date.getDate()));
-}
-
-export {timestampToDate, padWithZero};
